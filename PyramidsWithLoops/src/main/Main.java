dd6cdf101ce9f73d2f5334ab4955d628efc078a8
dorstedw <dorstedw@LCBLAB2110-43.uccob.uc.edu>
2019-09-19 09:33:44 -0400

    This is fixed. I am smart. Couldn't get the backspace command to work though

--- PyramidsWithLoops/src/main/Main.java
+++ PyramidsWithLoops/src/main/Main.java
@@ -3,27 +3,47 @@
  * nicholdw@ucmail.uc.edu
  * Printing pyramids using loops. It kind-of works. 
  * What I really want is
+ * 
+ * source for work: https://www.geeksforgeeks.org/programs-printing-pyramid-patterns-java/
+ * 
         1
        222
       33333
      4444444
     555555555 
     
-    
-    
-    dasdfafasdf
 */
 package main;
 
 public class Main {
 
 	public static void main(String[] args) {
-		int lines = 5;
-		for (int i = 0; i < lines; i++) {
-			for (int j = 0; j < i+1; j++) {
-				System.out.print(j+1);
-			}
-			System.out.println("");
-		}
+		
+		int n = 5;
+		int lineNum;
+		for (int i=0; i<n; i++) 
+		{ 
+
+			// inner loop to handle number spaces 
+			// values changing acc. to requirement 
+			for (int j=n-i; j>1; j--) 
+			{ 
+				// printing spaces 
+				System.out.print(" "); 
+			} 
+			lineNum = i+1;
+			System.out.print(lineNum);
+			//  inner loop to handle number of columns 
+			//  values changing acc. to outer loop 
+			for (int j=1; j<=i; j++ ) 
+			{ 
+          	 // printing number 
+				System.out.print(lineNum + "" + lineNum);
+			} 
+
+			// ending line after each row 
+			System.out.println(); 
+			
+		} 		
 	}
 }
