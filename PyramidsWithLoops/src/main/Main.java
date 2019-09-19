6818545e45c9e060d657b94a1febcf84e07541d1
dorstedw <dorstedw@LCBLAB2110-43.uccob.uc.edu>
2019-09-19 08:30:39 -0400

    learning how to commit

--- PyramidsWithLoops/src/main/Main.java
+++ PyramidsWithLoops/src/main/Main.java
@@ -17,7 +17,7 @@ public class Main {
 		int lines = 5;
 		for (int i = 0; i < lines; i++) {
 			for (int j = 0; j < i+1; j++) {
-				System.out.print(j);
+				System.out.print(j+1);
 			}
 			System.out.println("");
 		}
